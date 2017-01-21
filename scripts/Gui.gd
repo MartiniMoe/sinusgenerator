ca88fc71e51fcf2b40b108195c05123b02f768b2
MartiniMoe <martini.moe@cheex.de>
2017-01-21 14:54:48 +0100

    make gui more pretty

--- scripts/Gui.gd
+++ scripts/Gui.gd
@@ -1,20 +1,20 @@
 extends Control
 
 func _ready():
-	get_node("Waves/HSplitContainer/Wave1/Sliders/Frequency/Slider1f").set_value(gamemanager.wave1.frequency)
-	get_node("Waves/HSplitContainer/Wave2/Sliders/Frequency/Slider2f").set_value(gamemanager.wave2.frequency)
-	get_node("Waves/HSplitContainer2/Wave3/Sliders/Frequency/Slider3f").set_value(gamemanager.wave3.frequency)
-	get_node("Waves/HSplitContainer2/Wave4/Sliders/Frequency/Slider4f").set_value(gamemanager.wave4.frequency)
+	get_node("Wave1/Slider1f").set_value(gamemanager.wave1.frequency)
+	get_node("Wave2/Slider2f").set_value(gamemanager.wave2.frequency)
+	get_node("Wave3/Slider3f").set_value(gamemanager.wave3.frequency)
+	get_node("Wave4/Slider4f").set_value(gamemanager.wave4.frequency)
 
-	get_node("Waves/HSplitContainer/Wave1/Sliders/Amplitude/Slider1a").set_value(gamemanager.wave1.amplitude)
-	get_node("Waves/HSplitContainer/Wave2/Sliders/Amplitude/Slider2a").set_value(gamemanager.wave2.amplitude)
-	get_node("Waves/HSplitContainer2/Wave3/Sliders/Amplitude/Slider3a").set_value(gamemanager.wave3.amplitude)
-	get_node("Waves/HSplitContainer2/Wave4/Sliders/Amplitude/Slider4a").set_value(gamemanager.wave4.amplitude)
+	get_node("Wave1/Slider1a").set_value(gamemanager.wave1.amplitude)
+	get_node("Wave2/Slider2a").set_value(gamemanager.wave2.amplitude)
+	get_node("Wave3/Slider3a").set_value(gamemanager.wave3.amplitude)
+	get_node("Wave4/Slider4a").set_value(gamemanager.wave4.amplitude)
 	
-	get_node("Waves/HSplitContainer/Wave1/Sliders/Phase/Slider1p").set_value(gamemanager.wave1.angle_offset)
-	get_node("Waves/HSplitContainer/Wave2/Sliders/Phase/Slider2p").set_value(gamemanager.wave2.angle_offset)
-	get_node("Waves/HSplitContainer2/Wave3/Sliders/Phase/Slider3p").set_value(gamemanager.wave3.angle_offset)
-	get_node("Waves/HSplitContainer2/Wave4/Sliders/Phase/Slider4p").set_value(gamemanager.wave4.angle_offset)
+	get_node("Wave1/Slider1p").set_value(gamemanager.wave1.angle_offset)
+	get_node("Wave2/Slider2p").set_value(gamemanager.wave2.angle_offset)
+	get_node("Wave3/Slider3p").set_value(gamemanager.wave3.angle_offset)
+	get_node("Wave4/Slider4p").set_value(gamemanager.wave4.angle_offset)
 
 func _on_Slider1f_value_changed( value ):
 	gamemanager.wave1.set_freq(value)
