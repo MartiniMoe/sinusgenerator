887266df91787d607d91e0a3cd0d3e4c993ff7de
misterdanb <danb@hasi.it>
2017-01-22 16:58:32 +0100

    Fix levels

--- scripts/Wavegenerator.gd
+++ scripts/Wavegenerator.gd
@@ -15,7 +15,7 @@ var wave_material
 # leave this... it is needed to be 0 for the first child wave :D
 var current_pos = Vector2(0,0)
 
-var amplitudeArray = [100, 50, 25, 10]
+var amplitudeArray = [0, 0, 0, 0]
 var frequencyArray = [100, 180, 230, 390]
 var colorArray = [Color(0.4, 0.4, 0.6), Color(0.5, 0.5, 0.7), Color(0.6, 0.6, 0.8), Color(0.9, 0.9, 1.0)]
 
